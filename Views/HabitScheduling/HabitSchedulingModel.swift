5894562bd6bc334ca5fb1a77a4127c2b69a7ce01
Mathis Le Bonniec <mathislbonniec@gmail.com>
2024-02-13 17:17:01 +0100

    feat(Scheduling): create the Weekdays enum

--- Views/HabitScheduling/HabitSchedulingModel.swift
+++ Views/HabitScheduling/HabitSchedulingModel.swift
@@ -1,8 +1,14 @@
 //
-//  File.swift
-//  
+//  HabitSchedulingModel.swift
+//  Habits
 //
 //  Created by Mathis Le Bonniec on 13/02/2024.
 //
 
-import Foundation
+enum Weekdays: String, InlinePickerValue {
+  case monday, tuesday, wednesday, thursday, friday, saturday, sunday
+
+  var short: String {
+    String(self.rawValue.prefix(3)).capitalized
+  }
+}
