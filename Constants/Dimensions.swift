a23daac55d8727e6ce6ca4f6d0fc3535db42aeb6
Mathis Le Bonniec <mathislbonniec@gmail.com>
2024-02-13 13:11:27 +0100

    feat(Constants): create dimensions enum

---
+++ Constants/Dimensions.swift
@@ -0,0 +1,27 @@
+//
+//  Dimensions.swift
+//  Habits
+//
+//  Created by Mathis Le Bonniec on 10/02/2024.
+//
+
+import Foundation
+
+extension Constants {
+  enum Dimensions {
+    /// 3
+    static let tiny: CGFloat = 3
+
+    /// 6
+    static let small: CGFloat = 6
+
+    /// 12
+    static let medium: CGFloat = 12
+
+    /// 18
+    static let large: CGFloat = 18
+
+    /// 24
+    static let xlarge: CGFloat = 24
+  }
+}
