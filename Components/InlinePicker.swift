7c21f68611f574cdf91f0160d20b751b6f5329c3
Mathis Le Bonniec <mathislbonniec@gmail.com>
2024-02-13 17:16:40 +0100

    refactor(Picker): change WeekPicker to InlinePicker to allow passing enum conforming to a custom protocol

---
+++ Components/InlinePicker.swift
@@ -0,0 +1,89 @@
+//
+//  InlinePicker.swift
+//  Habits
+//
+//  Created by Mathis Le Bonniec on 12/02/2024.
+//
+
+import SwiftUI
+
+/// The protocol used for the InlinePicker component.
+/// Values displayed in the picker must conform to this protocol.
+protocol InlinePickerValue: RawRepresentable<String>, CaseIterable, Equatable {
+  /// A short string of the value, used to be displayed in the picker.
+  var short: String { get }
+}
+
+struct InlinePicker<Value: InlinePickerValue>: View {
+  // MARK: Properties
+  let values: [Value]
+  @Binding var selected: Value?
+
+  // MARK: Body
+  var body: some View {
+    LazyVGrid(
+      columns: Array(repeating: GridItem(.flexible()), count: values.count),
+      spacing: Constants.Dimensions.small
+    ) {
+      ForEach(values, id: \.rawValue) { value in
+        Button(action: {
+          withAnimation {
+            selected = selected == value ? nil : value
+          }
+          HapticHelper.selectionChanged()
+        }) {
+          getValueColor(isActive: selected == value)
+            .aspectRatio(1, contentMode: .fit)
+            .overlay {
+              Text(value.short)
+                .font(.caption)
+                .fontWeight(.semibold)
+                .foregroundStyle(selected == value ? .white : .black)
+            }
+            .clipShape(RoundedRectangle(cornerRadius: 15))
+            .shadow(color: .black.opacity(0.05), radius: 15, y: 10)
+            .overlay {
+              RoundedRectangle(cornerRadius: 15)
+                .stroke(Color(red: 229 / 255, green: 235 / 255, blue: 237 / 255), lineWidth: 1)
+                .aspectRatio(1, contentMode: .fill)
+            }
+        }
+        .buttonStyle(.plain)
+      }
+    }
+    .onAppear {
+      HapticHelper.selection.prepare()
+    }
+  }
+
+  // MARK: Private Method
+  func getValueColor(isActive: Bool) -> some View {
+    if !isActive {
+      return AnyView(Color.white)
+    }
+
+    return AnyView(
+      LinearGradient(
+        colors: [.accentColor.opacity(0.75), .accentColor],
+        startPoint: .top,
+        endPoint: .bottom
+      )
+    )
+  }
+}
+
+#Preview {
+  enum WeekdaysDemo: String, InlinePickerValue {
+    case monday, tuesday, wednesday, thursday, friday, saturday, sunday
+
+    var short: String {
+      String(self.rawValue.prefix(3)).capitalized
+    }
+  }
+
+  return InlinePicker(
+    values: WeekdaysDemo.allCases,
+    selected: .constant(.thursday)
+  )
+  .padding()
+}
---
+++ Views/HabitScheduling/HabitSchedulingModel.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Mathis Le Bonniec on 13/02/2024.
+//
+
+import Foundation
