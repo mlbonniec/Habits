74f7c633d5ec1dec9311133399b07482e9d06e14
Mathis Le Bonniec <mathislbonniec@gmail.com>
2024-02-13 14:43:34 +0100

    feat(HabitsHistory): create the history factory

---
+++ Views/HabitsHistory/HabitsHistoryFactory.swift
@@ -0,0 +1,12 @@
+//
+//  HabitsHistoryFactory.swift
+//  Habits
+//
+//  Created by Mathis Le Bonniec on 12/02/2024.
+//
+
+enum HabitsHistoryFactory {
+  static func createView() -> HabitsHistoryView {
+    HabitsHistoryView()
+  }
+}
