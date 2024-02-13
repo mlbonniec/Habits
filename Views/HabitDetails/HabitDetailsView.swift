5b168b34118194abf5a4f86722664883dac71e63
Mathis Le Bonniec <mathislbonniec@gmail.com>
2024-02-13 14:42:14 +0100

    feat(HabitsDetail): create the details view

---
+++ Views/HabitDetails/HabitDetailsView.swift
@@ -0,0 +1,116 @@
+//
+//  HabitDetailsView.swift
+//  Habits
+//
+//  Created by Mathis Le Bonniec on 11/02/2024.
+//
+
+import SwiftUI
+
+struct HabitDetailsView: View {
+  // MARK: Properties
+  private let habit: HabitsModel
+
+  // MARK: Reactive Properties
+  @Environment(\.dismiss) private var dismiss: DismissAction
+  @State private var steps: [Bool]
+  @State private var showScheduling: Bool = false
+
+  init(habit: HabitsModel) {
+    self.habit = habit
+    self._steps = State(initialValue: Array(repeating: false, count: habit.steps.count))
+  }
+
+  // MARK: Body
+  var body: some View {
+    ScrollView {
+      VStack(alignment: .leading, spacing: Constants.Dimensions.xlarge * 2) {
+        HabitsView
+        StepsView
+        ActionsView
+
+        Spacer(minLength: 0)
+      }
+      .padding(.horizontal, Constants.Dimensions.medium)
+    }
+    .backgroundGradient()
+    .navigationBarBackButtonHidden()
+    .navigationBarTitleDisplayMode(.inline)
+    .coordinateSpace(name: "scroll")
+    .toolbar {
+      ToolbarItem(placement: .topBarTrailing) {
+        CloseButton(action: dismiss.callAsFunction)
+      }
+    }
+    .sheet(isPresented: $showScheduling) {
+      HabitSchedulingFactory.createView()
+    }
+  }
+
+  // MARK: Private Views
+  private var HabitsView: some View {
+    GroupBox("Habit") {
+      HabitsListRowView(
+        systemImage: habit.systemImage,
+        label: habit.label,
+        description: habit.description
+      )
+      .hideChevron()
+    }
+    .groupBoxStyle(GroupBoxHabitStyle())
+  }
+
+  private var StepsView: some View {
+    GroupBox("Steps") {
+      ForEach(Array(habit.steps.enumerated()), id: \.offset) { id, step in
+        HabitDetailsItemView(
+          label: step.label,
+          description: step.description,
+          isOn: $steps[id]
+        )
+        .opacity(id == 0 || steps[id - 1] ? 1 : 0.5)
+        .allowsHitTesting(id == 0 || (id != 0 && steps[id - 1]))
+      }
+      .onChange(of: steps) { _ in
+        var hasFoundLatestFalse: Bool = false
+
+        steps.enumerated().forEach { id, step in
+          if !step && !hasFoundLatestFalse {
+            hasFoundLatestFalse = true
+          }
+          if hasFoundLatestFalse {
+            steps[id] = false
+          }
+        }
+
+        if steps.allSatisfy({ $0 }) {
+          HapticHelper.notification.prepare()
+        }
+      }
+    }
+    .groupBoxStyle(GroupBoxHabitStyle())
+  }
+
+  private var ActionsView: some View {
+    GroupBox("Actions") {
+      Button("Mark as completed") {
+        HapticHelper.success()
+        dismiss()
+      }
+      .buttonStyle(ButtonHabitStyle())
+      .disabled(steps.contains { !$0 })
+
+      Button("Schedule on a day") {
+        showScheduling.toggle()
+      }
+      .buttonStyle(ButtonHabitStyle(color: Color(red: 238/255, green: 66/255, blue: 102/255)))
+    }
+    .groupBoxStyle(GroupBoxHabitStyle())
+  }
+}
+
+#Preview {
+  let habit: HabitsModel = HabitsMapper.mock()
+
+  return HabitDetailsFactory.createView(habit: habit)
+}
