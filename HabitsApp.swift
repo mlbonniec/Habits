5da2c3375110a59690a7d57e099e234efe953da9
Mathis Le Bonniec <mathislbonniec@gmail.com>
2024-02-13 14:43:46 +0100

    feat(HabitsApp): create the main app struct

---
+++ HabitsApp.swift
@@ -0,0 +1,46 @@
+//
+//  HabitsApp.swift
+//  Habits
+//
+//  Created by Mathis Le Bonniec on 10/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct HabitsApp: App {
+  // MARK: Properties
+  @State private var activeTab: AppTabView = .habits
+
+  // MARK: Body
+  var body: some Scene {
+    WindowGroup {
+      TabView(selection: $activeTab) {
+        HabitsListFactory.createView()
+          .tabItem(
+            label: "Habits",
+            systemImage: "checklist",
+            activeSystemImage: "checklist.checked",
+            tag: .habits,
+            activeTab: activeTab
+          )
+
+        Text("Schedule")
+          .tabItem(
+            label: "Schedule",
+            systemImage: "calendar",
+            tag: .calendar,
+            activeTab: activeTab
+          )
+
+        HabitsHistoryFactory.createView()
+          .tabItem(
+            label: "History",
+            systemImage: "clock.arrow.circlepath",
+            tag: .history,
+            activeTab: activeTab
+          )
+      }
+    }
+  }
+}
