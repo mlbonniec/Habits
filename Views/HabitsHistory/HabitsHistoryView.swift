23db187ffc4bf6e6608752405b11057800e953ed
Mathis Le Bonniec <mathislbonniec@gmail.com>
2024-02-13 14:43:30 +0100

    feat(HabitsHistory): create the history view

---
+++ Views/HabitsHistory/HabitsHistoryView.swift
@@ -0,0 +1,45 @@
+//
+//  HabitsHistoryView.swift
+//  Habits
+//
+//  Created by Mathis Le Bonniec on 12/02/2024.
+//
+
+import SwiftUI
+
+struct HabitsHistoryView: View {
+  // MARK: Properties
+  // TODO: Replace with real data
+  private let data: [HabitsModel] = HabitsMapper.map()
+
+  // MARK: Body
+  var body: some View {
+    Group {
+      if data.isEmpty {
+        HabitsHistoryNoDataView()
+      } else {
+        HistoryList
+      }
+    }
+    .navigable("History")
+  }
+
+  // MARK: Private Views
+  private var HistoryList: some View {
+    List(data) { habit in
+      HabitsListRowView(
+        systemImage: habit.systemImage,
+        label: habit.label,
+        description: Date().formatted()
+      )
+      .hideChevron()
+      .habitListRow()
+    }
+    .habitList()
+    .backgroundGradient()
+  }
+}
+
+#Preview {
+  HabitsHistoryView()
+}
