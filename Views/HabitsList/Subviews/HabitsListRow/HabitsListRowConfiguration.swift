2474fb4856fa83c1e9fbd2f207b88bfaf81c85de
Mathis Le Bonniec <mathislbonniec@gmail.com>
2024-02-13 14:41:12 +0100

    feat(HabitsListRow): create the row configuration

---
+++ Views/HabitsList/Subviews/HabitsListRow/HabitsListRowConfiguration.swift
@@ -0,0 +1,12 @@
+//
+//  HabitsListRowConfiguration.swift
+//  Habits
+//
+//  Created by Mathis Le Bonniec on 11/02/2024.
+//
+
+import Foundation
+
+class HabitsListRowConfiguration: ObservableObject {
+  var showChevron: Bool = true
+}
