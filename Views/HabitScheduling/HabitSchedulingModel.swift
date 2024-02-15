a84883e566a6a2dd664c19a8ea99215559195e56
Mathis Le Bonniec <mathislbonniec@gmail.com>
2024-02-16 00:42:28 +0100

    feat(weekdays): now show the real weekday instead of mock

--- Views/HabitScheduling/HabitSchedulingModel.swift
+++ Views/HabitScheduling/HabitSchedulingModel.swift
@@ -17,4 +17,25 @@ enum Weekdays: String, Identifiable, PersistableEnum, InlinePickerValue {
   var short: String {
     String(self.rawValue.prefix(3)).capitalized
   }
+
+  init?(rawValue: Int?) {
+    switch rawValue {
+    case 1:
+      self = .monday
+    case 2:
+      self = .tuesday
+    case 3:
+      self = .wednesday
+    case 4:
+      self = .thursday
+    case 5:
+      self = .friday
+    case 6:
+      self = .saturday
+    case 7:
+      self = .sunday
+    default:
+      return nil
+    }
+  }
 }
--- Views/HabitsSchedule/HabitsScheduleView.swift
+++ Views/HabitsSchedule/HabitsScheduleView.swift
@@ -9,12 +9,28 @@ import RealmSwift
 import SwiftUI
 
 struct HabitsScheduleView: View {
-  // MARK: Private Properties
-  private let currentWeekday: Weekdays = .thursday
-  
   // MARK: Reactive Properties
   @ObservedResults(HabitsScheduleModel.self) private var schedule: Results<HabitsScheduleModel>
   @State private var selectedWeekday: Weekdays?
+
+  // MARK: Private Methods
+  var selectedDays: [Weekdays] {
+    if let selectedWeekday {
+      return [selectedWeekday]
+    }
+
+    return Weekdays.allCases
+  }
+
+  var currentWeekday: Weekdays? {
+    let date = Calendar.current.dateComponents([.weekday], from: Date.now)
+    let weekday = date.weekday
+
+    if let weekday {
+      return Weekdays(rawValue: weekday - 1)
+    }
+    return nil
+  }
 
   // MARK: Body
   var body: some View {
@@ -23,39 +39,38 @@ struct HabitsScheduleView: View {
         InlinePicker(
           values: Weekdays.allCases,
           selected: $selectedWeekday,
-          indicators: [currentWeekday]
+          indicators: Set([currentWeekday].compactMap { $0 })
         )
 
-        ForEach(Weekdays.allCases) { day in
-          if selectedWeekday == nil || day == selectedWeekday {
-            GroupBox {
-              let filteredIds: [HabitsIds] = schedule.filter { $0.days.contains(day) }.map { $0.id }
-              if filteredIds.isEmpty {
-                Text("Nothing scheduled yet.")
-                  .frame(maxWidth: .infinity, alignment: .leading)
-                  .font(.caption2)
-                  .italic()
-                  .foregroundStyle(.gray)
-                  .padding(.leading, Constants.Dimensions.medium)
-              } else {
-                ForEach(filteredIds) { id in
-                  if let habit: HabitsModel = HabitsMapper.mapById(id: id) {
-                    HabitsListRowView(
-                      systemImage: habit.systemImage,
-                      label: habit.id.rawValue,
-                      description: habit.description
-                    )
-                  }
+        ForEach(selectedDays) { day in
+          GroupBox {
+            let filteredIds: [HabitsIds] = schedule.filter { $0.days.contains(day) }.map { $0.id }
+            if filteredIds.isEmpty {
+              Text("Nothing scheduled yet.")
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .font(.caption2)
+                .italic()
+                .foregroundStyle(.gray)
+                .padding(.leading, Constants.Dimensions.medium)
+            } else {
+              ForEach(filteredIds) { id in
+                if let habit: HabitsModel = HabitsMapper.mapById(id: id) {
+                  HabitsListRowView(
+                    systemImage: habit.systemImage,
+                    label: habit.id.rawValue,
+                    description: habit.description
+                  )
                 }
               }
-            } label: {
-              Text(day.rawValue.capitalized)
-                .foregroundStyle(day == currentWeekday ? Color.accentColor : .black)
             }
+          } label: {
+            Text(day.rawValue.capitalized)
+              .foregroundStyle(day == currentWeekday ? Color.accentColor : .black)
           }
+          .groupBoxStyle(GroupBoxHabitStyle())
+          .padding(.bottom, day == selectedDays.last ? Constants.Dimensions.xxlarge : 0)
         }
       }
-      .groupBoxStyle(GroupBoxHabitStyle())
       .habitListRow()
     }
     .habitList(spacing: Constants.Dimensions.xxlarge)
