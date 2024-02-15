eb0db8aa9e3bbf152b8f6dda8b9bbe3ce9520012
Mathis Le Bonniec <mathislbonniec@gmail.com>
2024-02-15 16:55:05 +0100

    feat(schedule): create the schedule view

--- Views/HabitsSchedule/HabitsScheduleView.swift
+++ Views/HabitsSchedule/HabitsScheduleView.swift
@@ -1,18 +1,54 @@
 //
-//  SwiftUIView.swift
-//  
+//  HabitsScheduleView.swift
+//  Habits
 //
 //  Created by Mathis Le Bonniec on 14/02/2024.
 //
 
 import SwiftUI
 
-struct SwiftUIView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct HabitsScheduleView: View {
+  // MARK: Private Properties
+  private let currentWeekday: Weekdays = .thursday
+  
+  // MARK: Reactive Properties
+  @State private var selectedWeekday: Weekdays?
+
+  // MARK: Body
+  var body: some View {
+    List {
+      Group {
+        InlinePicker(
+          values: Weekdays.allCases,
+          selected: $selectedWeekday,
+          indicators: [currentWeekday]
+        )
+
+        ForEach(Weekdays.allCases) { day in
+          if selectedWeekday == nil || day == selectedWeekday {
+            GroupBox {
+              Text("Nothing scheduled yet.")
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .font(.caption2)
+                .italic()
+                .foregroundStyle(.gray)
+                .padding(.leading, Constants.Dimensions.medium)
+            } label: {
+              Text(day.rawValue.capitalized)
+                .foregroundStyle(day == currentWeekday ? Color.accentColor : .black)
+            }
+          }
+        }
+      }
+      .groupBoxStyle(GroupBoxHabitStyle())
+      .habitListRow()
     }
+    .habitList(spacing: Constants.Dimensions.xxlarge)
+    .backgroundGradient()
+    .navigable("Schedule")
+  }
 }
 
 #Preview {
-    SwiftUIView()
+  HabitsScheduleView()
 }
