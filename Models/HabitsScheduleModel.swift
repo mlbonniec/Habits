a1d5b6781c10aba3a71e267eda8f1a61f24c857d
Mathis Le Bonniec <mathislbonniec@gmail.com>
2024-02-15 17:08:25 +0100

    feat(schedule): create HabitsScheduleModel

--- Models/HabitsScheduleModel.swift
+++ Models/HabitsScheduleModel.swift
@@ -1,8 +1,21 @@
 //
-//  File.swift
-//  
+//  HabitsScheduleModel.swift
+//  Habits
 //
 //  Created by Mathis Le Bonniec on 15/02/2024.
 //
 
 import Foundation
+import RealmSwift
+
+class HabitsScheduleModel: Object {
+  @Persisted var id: HabitsIds
+  @Persisted var days: List<Weekdays>
+
+  convenience init(id: HabitsIds, days: [Weekdays]) {
+    self.init()
+    self.id = id
+    self.days = List()
+    self.days.append(objectsIn: days)
+  }
+}
