1151dde3f1b52c738e57f370de83215aa430fecc
Mathis Le Bonniec <mathislbonniec@gmail.com>
2024-02-18 22:34:36 +0100

    fix(box): change shadow color

--- Modifiers/BoxAppearance.swift
+++ Modifiers/BoxAppearance.swift
@@ -11,20 +11,12 @@ struct BoxAppearance: ViewModifier {
   // MARK: Properties
   let cornerRadius: CGFloat
 
-  // MARK: Reactive Properties
-  @Environment(\.colorScheme) private var colorScheme: ColorScheme
-
-  // MARK: Computed Properties
-  private var shadowOpacity: CGFloat {
-    colorScheme == .dark ? 0.025 : 0.05
-  }
-
   // MARK: Body
   func body(content: Content) -> some View {
     content
       .background(Constants.Colors.primary)
       .clipShape(shape)
-      .shadow(color: Color.primary.opacity(shadowOpacity), radius: 15, y: 10)
+      .shadow(color: Constants.Colors.text.opacity(0.025), radius: 15, y: 10)
       .overlay {
         shape
           .stroke(Constants.Colors.stroke, lineWidth: 1)
