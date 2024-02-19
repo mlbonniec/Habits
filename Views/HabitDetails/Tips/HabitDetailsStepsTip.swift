c77d1c2e8018b32d67654ac1e1750538af388da6
Mathis Le Bonniec <mathislbonniec@gmail.com>
2024-02-19 22:51:20 +0100

    feat(tip): create the details steps tip

---
+++ Views/HabitDetails/Tips/HabitDetailsStepsTip.swift
@@ -0,0 +1,28 @@
+//
+//  HabitDetailsStepsTip.swift
+//  Habits
+//
+//  Created by Mathis Le Bonniec on 19/02/2024.
+//
+
+import SwiftUI
+import TipKit
+
+@available(iOS 17.0, *)
+struct HabitDetailsStepsTip: Tip {
+  var id: String {
+    "checkSteps"
+  }
+
+  var title: Text {
+    Text("Check steps")
+  }
+
+  var message: Text? {
+    Text("Once a step has been completed, don't forget to check it. You'll then be able to \"Mark as completed\"")
+  }
+
+  var image: Image? {
+    Image(systemName: "checkmark.circle.fill")
+  }
+}
