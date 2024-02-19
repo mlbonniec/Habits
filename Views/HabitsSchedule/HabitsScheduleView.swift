98676499aeff6c0fc52d515462201e63739a8309
Mathis Le Bonniec <mathislbonniec@gmail.com>
2024-02-19 22:56:07 +0100

    refactor(Schedule): remove unnecessary Group in List

--- Views/HabitsSchedule/HabitsScheduleView.swift
+++ Views/HabitsSchedule/HabitsScheduleView.swift
@@ -26,45 +26,43 @@ struct HabitsScheduleView: View {
   // MARK: Body
   var body: some View {
     List {
-      Group {
-        InlinePicker(
-          values: Weekdays.orderedAllCases,
-          selected: $selectedWeekday,
-          indicators: Set([
-            Calendar.currentWeekday
-          ].compactMap { $0 })
-        )
+      InlinePicker(
+        values: Weekdays.orderedAllCases,
+        selected: $selectedWeekday,
+        indicators: Set([
+          Calendar.currentWeekday
+        ].compactMap { $0 })
+      )
 
-        ForEach(selectedDays) { day in
-          GroupBox {
-            let filteredIds: [HabitsIds] = schedule.filter { $0.days.contains(day) }.map { $0.id }
-            if filteredIds.isEmpty {
-              Text("Nothing scheduled yet.")
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .font(.caption2)
-                .italic()
-                .foregroundStyle(.gray)
-                .padding(.leading, Constants.Dimensions.medium)
-            } else {
-              ForEach(filteredIds) { id in
-                if let habit: HabitsModel = HabitsMapper.mapById(id: id) {
-                  Button(action: { selectedHabit = habit }) {
-                    HabitsListRowView(
-                      systemImage: habit.systemImage,
-                      label: habit.id.rawValue,
-                      description: habit.description
-                    )
-                  }
+      ForEach(selectedDays) { day in
+        GroupBox {
+          let filteredIds: [HabitsIds] = schedule.filter { $0.days.contains(day) }.map { $0.id }
+          if filteredIds.isEmpty {
+            Text("Nothing scheduled yet.")
+              .frame(maxWidth: .infinity, alignment: .leading)
+              .font(.caption2)
+              .italic()
+              .foregroundStyle(.gray)
+              .padding(.leading, Constants.Dimensions.medium)
+          } else {
+            ForEach(filteredIds) { id in
+              if let habit: HabitsModel = HabitsMapper.mapById(id: id) {
+                Button(action: { selectedHabit = habit }) {
+                  HabitsListRowView(
+                    systemImage: habit.systemImage,
+                    label: habit.id.rawValue,
+                    description: habit.description
+                  )
                 }
               }
             }
-          } label: {
-            Text(day.rawValue.capitalized)
-              .foregroundStyle(day == Calendar.currentWeekday ? Color.accentColor : Constants.Colors.text)
           }
-          .groupBoxStyle(GroupBoxHabitStyle())
-          .padding(.bottom, day == selectedDays.last ? Constants.Dimensions.xxlarge : 0)
+        } label: {
+          Text(day.rawValue.capitalized)
+            .foregroundStyle(day == Calendar.currentWeekday ? Color.accentColor : Constants.Colors.text)
         }
+        .groupBoxStyle(GroupBoxHabitStyle())
+        .padding(.bottom, day == selectedDays.last ? Constants.Dimensions.xxlarge : 0)
       }
       .habitListRow()
     }
